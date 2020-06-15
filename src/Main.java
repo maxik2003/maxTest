5964daab3579663c92d1288b64abd74a89f94332
Yegor Sherbatkin <Scherbatkin@databriz.ru>
2020-06-15 21:35:37 +0300

    Научились перебирать массивы массивов

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String[][] texts = {
+                {"Иаков 1:1", "Иаков 1:2", "Иаков 1:3" ,"Иаков 1:4"},
+                {"Иаков 2:1", "Иаков 2:2"},
+                {"Иаков 3:1", "Иаков 3:2", "Иаков 3:3", "Иаков 3:4"}
+        };
+        for ( byte a = 0; a < texts.length ; a++ ) {
+            for ( byte i = 0; i < texts[a].length ; i++ ) {
+                System.out.println(texts[a][i]);
+            }
+        }
     }
 }
